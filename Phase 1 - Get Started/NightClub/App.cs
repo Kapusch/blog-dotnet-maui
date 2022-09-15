ab0d71e31896419d208f0a9cdb400e76e0b3b95c
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-09-15 21:26:59 +0200

    Replace App.xaml file with its proper App.cs

---
+++ Phase 1 - Get Started/NightClub/App.cs
@@ -0,0 +1,11 @@
+﻿using NightClub.Views;
+
+namespace NightClub;
+
+public partial class App : Application
+{
+    public App()
+    {
+        MainPage = new NavigationPage(new HomePage());
+    }
+}

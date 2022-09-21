cfd9ba19f9f8dfd5e755e4e0427b78f08649df67
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-09-21 18:20:07 +0200

    Mark ViewModel partial and observable

--- Phase 1 - Get Started/NightClub/ViewModels/HomeViewModel.cs
+++ Phase 1 - Get Started/NightClub/ViewModels/HomeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace NightClub.ViewModels
 {
-    public class HomeViewModel
+    public partial class HomeViewModel : ObservableObject
     {
         public HomeViewModel()
         {

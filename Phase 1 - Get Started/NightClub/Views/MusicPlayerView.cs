6c699f31cdce597749f7095fff2a87b50fb2670e
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-09-24 19:39:24 +0200

    Ajout de la page principale

---
+++ Phase 1 - Get Started/NightClub/Views/MusicPlayerView.cs
@@ -0,0 +1,15 @@
+﻿namespace NightClub.Views;
+
+public class MusicPlayerView : ContentPage
+{
+	public MusicPlayerView()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}

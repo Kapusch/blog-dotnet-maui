4d36711c97c4521025bc14244c4d5b0f02fbdd6a
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-10-02 19:46:05 +0200

    Displaying the time tracker row

--- Phase 1 - Get Started/NightClub/Views/MusicPlayerView.cs
+++ Phase 1 - Get Started/NightClub/Views/MusicPlayerView.cs
@@ -27,6 +27,8 @@ public class MusicPlayerView : ContentPage
 
     #region Controls
 
+    #region Main Layouts
+
     Grid TopLayout => new Grid
     {
         BackgroundColor = Colors.Black
@@ -51,29 +53,63 @@ public class MusicPlayerView : ContentPage
         ColumnSpacing = 0,
         Children =
         {
-            new BoxView() { Color = Color.FromArgb("#ffffff") }.Row(0).Column(0),
-            new BoxView() { Color = Color.FromArgb("#d0d0d0") }.Row(0).Column(1),
-            new BoxView() { Color = Color.FromArgb("#a2a3a3") }.Row(0).Column(2),
-            new BoxView() { Color = Color.FromArgb("#777879") }.Row(0).Column(3),
-            new BoxView() { Color = Color.FromArgb("#4e5051") }.Row(0).Column(4),
-            new BoxView() { Color = Color.FromArgb("#292b2c") }.Row(0).Column(5),
-            new BoxView() { Color = Color.FromArgb("#000405") }.Row(0).Column(6),
-            new BoxView() { Color = Color.FromArgb("#f3f337") }.Row(1).Column(0),
-            new BoxView() { Color = Color.FromArgb("#a2eb5b") }.Row(1).Column(1),
-            new BoxView() { Color = Color.FromArgb("#4edb80") }.Row(1).Column(2),
-            new BoxView() { Color = Color.FromArgb("#00c89f") }.Row(1).Column(3),
-            new BoxView() { Color = Color.FromArgb("#00b1b1") }.Row(1).Column(4),
-            new BoxView() { Color = Color.FromArgb("#0098b2") }.Row(1).Column(5),
-            new BoxView() { Color = Color.FromArgb("#177ea2") }.Row(1).Column(6),
-            new BoxView() { Color = Color.FromArgb("#bf7aef") }.Row(2).Column(0),
-            new BoxView() { Color = Color.FromArgb("#ea6cd4") }.Row(2).Column(1),
-            new BoxView() { Color = Color.FromArgb("#ff63b3") }.Row(2).Column(2),
-            new BoxView() { Color = Color.FromArgb("#ff6590") }.Row(2).Column(3),
-            new BoxView() { Color = Color.FromArgb("#ff716e") }.Row(2).Column(4),
-            new BoxView() { Color = Color.FromArgb("#ff844e") }.Row(2).Column(5),
-            new BoxView() { Color = Color.FromArgb("#f89832") }.Row(2).Column(6),
+            ElapsedTime.Row(0).Column(1),
+            TimeTracker.Row(0).Column(2).ColumnSpan(3),
+            RemainingTime.Row(0).Column(5)
         }
     };
+
+    #endregion
+
+    #region Time Tracker
+
+    Label ElapsedTime => new Label
+    {
+        FontSize = 14,
+        Text = "0:36",
+        TextColor = Colors.White
+    }.TextCenter();
+
+    Slider TimeTracker => new Slider
+    {
+        Minimum = 0,
+        MinimumTrackColor = Colors.LightSalmon,
+        Maximum = 100,
+        MaximumTrackColor = Colors.Black,
+        Value = 20
+    };
+
+    /*
+            <Slider
+                x:Name="timeTracker"
+                Grid.Row="0"
+                Grid.Column="1"
+                DragCompleted="TimeTracker_DragCompleted"
+                DragStarted="TimeTracker_DragStarted"
+                Maximum="{Binding Duration, Converter={StaticResource TimeSpanConverter}, Source={x:Reference mediaPlayer}}"
+                MaximumTrackColor="{StaticResource Black}"
+                MinimumTrackColor="{StaticResource LightSalmonPink}" />*/
+
+    Label RemainingTime => new Label
+    {
+        FontSize = 14,
+        Text = "2:57",
+        TextColor = Colors.White
+    }.TextCenter();
+
+    #endregion
+
+    #region Media Control Panel
+
+
+
+    #endregion
+
+    #region Volume Tracker
+
+
+
+    #endregion
 
     #endregion
 

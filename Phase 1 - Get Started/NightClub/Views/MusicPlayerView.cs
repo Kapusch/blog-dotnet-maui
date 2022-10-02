da658604fe884383c5eaaa3f47a223150b27df1f
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-10-02 17:23:25 +0200

    Scaffolding the bottom layout

--- Phase 1 - Get Started/NightClub/Views/MusicPlayerView.cs
+++ Phase 1 - Get Started/NightClub/Views/MusicPlayerView.cs
@@ -9,6 +9,7 @@ public class MusicPlayerView : ContentPage
         Console.WriteLine("[NightClub] MusicPlayerView - Constructor");
 
         NavigationPage.SetHasNavigationBar(this, false);
+        BackgroundColor = Colors.DimGray;
 
         Content = new Grid
         {
@@ -33,7 +34,45 @@ public class MusicPlayerView : ContentPage
 
     Grid BottomLayout => new Grid
     {
-        BackgroundColor = Colors.DimGray
+        BackgroundColor = Colors.DimGray,
+        RowDefinitions = Rows.Define(
+                Stars(1),
+                Stars(1),
+                Stars(1)),
+        RowSpacing = 0,
+        ColumnDefinitions = Columns.Define(
+                Stars(10),
+                Stars(10),
+                Stars(20),
+                Stars(20),
+                Stars(20),
+                Stars(10),
+                Stars(10)),
+        ColumnSpacing = 0,
+        Children =
+        {
+            new BoxView() { Color = Color.FromArgb("#ffffff") }.Row(0).Column(0),
+            new BoxView() { Color = Color.FromArgb("#d0d0d0") }.Row(0).Column(1),
+            new BoxView() { Color = Color.FromArgb("#a2a3a3") }.Row(0).Column(2),
+            new BoxView() { Color = Color.FromArgb("#777879") }.Row(0).Column(3),
+            new BoxView() { Color = Color.FromArgb("#4e5051") }.Row(0).Column(4),
+            new BoxView() { Color = Color.FromArgb("#292b2c") }.Row(0).Column(5),
+            new BoxView() { Color = Color.FromArgb("#000405") }.Row(0).Column(6),
+            new BoxView() { Color = Color.FromArgb("#f3f337") }.Row(1).Column(0),
+            new BoxView() { Color = Color.FromArgb("#a2eb5b") }.Row(1).Column(1),
+            new BoxView() { Color = Color.FromArgb("#4edb80") }.Row(1).Column(2),
+            new BoxView() { Color = Color.FromArgb("#00c89f") }.Row(1).Column(3),
+            new BoxView() { Color = Color.FromArgb("#00b1b1") }.Row(1).Column(4),
+            new BoxView() { Color = Color.FromArgb("#0098b2") }.Row(1).Column(5),
+            new BoxView() { Color = Color.FromArgb("#177ea2") }.Row(1).Column(6),
+            new BoxView() { Color = Color.FromArgb("#bf7aef") }.Row(2).Column(0),
+            new BoxView() { Color = Color.FromArgb("#ea6cd4") }.Row(2).Column(1),
+            new BoxView() { Color = Color.FromArgb("#ff63b3") }.Row(2).Column(2),
+            new BoxView() { Color = Color.FromArgb("#ff6590") }.Row(2).Column(3),
+            new BoxView() { Color = Color.FromArgb("#ff716e") }.Row(2).Column(4),
+            new BoxView() { Color = Color.FromArgb("#ff844e") }.Row(2).Column(5),
+            new BoxView() { Color = Color.FromArgb("#f89832") }.Row(2).Column(6),
+        }
     };
 
     #endregion

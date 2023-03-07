92b9341a57ed6ecbbbef5b209231cc80622bffe1
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2023-03-08 00:02:56 +0100

    Fix file-scoped namespace

--- Samples/NightClub/4 - Media Control/NightClub/ViewModels/HomeViewModel.cs
+++ Samples/NightClub/4 - Media Control/NightClub/ViewModels/HomeViewModel.cs
@@ -3,20 +3,18 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NightClub.Views;
 
-namespace NightClub.ViewModels
+namespace NightClub.ViewModels;
+public partial class HomeViewModel : ObservableObject
 {
-    public partial class HomeViewModel : ObservableObject
+    public HomeViewModel()
     {
-        public HomeViewModel()
-        {
-        }
+    }
 
-        [RelayCommand]
-        async Task Enter()
-        {
-            await Application.Current.MainPage.Navigation.PushAsync(
-                new MusicPlayerView());
-        }
+    [RelayCommand]
+    async Task Enter()
+    {
+        await Application.Current.MainPage.Navigation.PushAsync(
+            new MusicPlayerView());
     }
 }
 
